395960996dfb54f8f74823c3918c25114006ed0e
Brian Mwiruki <mwiruki@outlook.com>
2019-10-28 07:49:27 +0300

    border animate

--- js/script.js
+++ js/script.js
@@ -1,23 +1,27 @@
 function akan() {
-  let day = parseInt(document.getElementById("day").value);
-  let month = parseInt(document.getElementById("month").value);
+  let day = document.getElementById("day").value;
+  let month = document.getElementById("month").value;
   let year = document.getElementById("year").value;
   let cc = parseInt(year.slice(0, 2));
   let yy = parseInt(year.slice(2, 4));
   let sex = document.getElementById("gender").value;
+  // let fml =( ( (cc/4) - 2 * (cc - 1)) + ((5*yy/4) ) + ((26*(month+1)/10)) + day )% 7;
+  // let bdate = fml.toFixed(0);
+  // let bd = bdate - 1;
+  // let date = Math.abs(bd);
+  // console.log(bdate);
+  let gdate = new Date(`${year}-${month}-${day}`);
+  let b = gdate.getDay();
+  console.log(b);
 
-  let fml =( ( (cc/4) - 2 * (cc - 1)) + ((5*yy/4) ) + ((26*(month+1)/10)) + day )% 7;
-  let bdate = fml.toFixed(0);
-  let bd = bdate - 1;
-  let date = Math.abs(bd);
-  console.log(bdate);
     // return document.getElementById("res").innerHTML= bdate;
     let mname = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
     let fname = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
   if (sex == "male"){
-    return  document.getElementById("result").innerHTML = mname[date];
+    return  document.getElementById("result").innerHTML = mname[b];
   }
-    if (sex == "female"){
-      return  document.getElementById("result").innerHTML = fname[date];
+  if (sex == "female")
+  {
+      return  document.getElementById("result").innerHTML = fname[b];
     }
 }

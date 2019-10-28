e557ba384c6e23fed71a6a0d15e429155dbc4741
Brian Mwiruki <mwiruki@outlook.com>
2019-10-28 13:15:50 +0300

    indentation and spacing

--- js/script.js
+++ js/script.js
@@ -2,15 +2,9 @@ function akan() {
   let day = document.getElementById("day").value;
   let month = document.getElementById("month").value;
   let year = document.getElementById("year").value;
-  // let cc = parseInt(year.slice(0, 2));
-  // let yy = parseInt(year.slice(2, 4));
   let sex = document.getElementById("gender").value;
   let mname = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
   let fname = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-  // let fml =( ( (cc/4) - 2 * (cc - 1)) + ((5*yy/4) ) + ((26*(month+1)/10)) + day )% 7;
-  // let bdate = fml.toFixed(0);
-  // let bd = bdate - 1;
-  // let date = Math.abs(bd);
   if (day < 1 || day > 31) {
     return  document.getElementById("result").innerHTML = "Enter Valid Date";
   }
@@ -34,3 +28,9 @@ function akan() {
       return  document.getElementById("result").innerHTML = fname[b];
     }
 }
+// let cc = parseInt(year.slice(0, 2));
+// let yy = parseInt(year.slice(2, 4));
+// let fml =( ( (cc/4) - 2 * (cc - 1)) + ((5*yy/4) ) + ((26*(month+1)/10)) + day )% 7;
+// let bdate = fml.toFixed(0);
+// let bd = bdate - 1;
+// let date = Math.abs(bd);
